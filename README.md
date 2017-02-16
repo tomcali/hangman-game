2ec79840305d3eecd3d56ad4dfe114325cccdd6c
tomcali <tmiller@toutbay.com>
2017-02-16 12:22:52 -0800

    hangman game logic implemented with sets

--- README.md
+++ README.md
@@ -6,6 +6,13 @@ This assignment involves building a hangman-style game. It provides an opportuni
 
 The variation on the design that I am trying is to use the game as a statistics training device. A set of statistical terms, hints, and full definitions is maintained in a JSON data structure, which may be sourced into the JavaScript program creating a JavaScript object that we can use to guide the game. The JSON data are limited in this initial version of the game, which is merely a prototype or demonstration. But instructors could easily add to the data structure providing an extensive array of statistical terms for training purposes.
 
+The main thing that I am doing regarding the logic of the game is using set operations. I define a set object following documentation from
+Mozilla at
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/
+
+Sets make implementing hangman game logic so much easier. I focused on the game logic on this assignment. Not really concerned about how good the final game looks. Function over form is the rule for now. To check out the function, I use lots of console.log() functions. I include a file of set functions as well, though I did not need to use these for the hangman game. Good to have them handy, though, for future projects.
+
 I continue to work with Bootstrap 4, which is currently in alpha release. So far my testing of this shows that Bootstrap 4 operates about the same as Bootstrap 3. Documentation for Bootstrap 4 suggests that we should employ stlying best practices of normalize.css, such as using em rather than px for size paramters. Bootstrap 4 is actually a smaller package because it drops glyphs/icons included with Bootstrap 3, so these must be included as separate icon/glyph files. Bootstrap 4 also has an extra small setting for responsiveness, making solutions compatible with very small screens. If you interact with the web through your own version of a Dick Tracy watch, whether the product of Apple or Google, you may be better served by Bootstrap 4. 
 
 ## Viewing the Websites
@@ -21,9 +28,9 @@ The game is a hangman style game displayed with a Bootstrap Jumbotron at the top
 - HTML
 - CSS (style.css after Bootstrap 4 css and Yeti Bootswatch)
 - JavaScript (via Bootstrap 4) and hangman code
+- Set data structures implemented via JavaScript objects
 - Git/GitHub
-- Media Queries
-- Bootstrap
+- Bootstrap for responsive design
 - One image used as background for the Bootstrap Jumbotron
 
 ## Code Explanation
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,14 +6,103 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-
 // readGameData.js contains an array of JSON objects for the game items
+console.log('the game data represent an object with statistical terms,');
+console.log('short hints, and complete definitions of the terms.,');
 console.log(gameData);
 
 // choose an term at random
-var indexOfTerm = getRandomInt(0, gameData.length - 1)
-selectedTerm = gameData[indexOfTerm]
+var indexOfTerm = getRandomInt(0, gameData.length - 1);
+var selectedTerm = gameData[indexOfTerm];
 
-console.log('selected term:', selectedTerm['term'])
-console.log('selected hint:', selectedTerm['hint'])
-console.log('selected definition:', selectedTerm['definition'])
+console.log('selected term:', selectedTerm['term']);
+console.log('selected hint:', selectedTerm['hint']);
+console.log('selected definition:', selectedTerm['definition']);
+
+var numberTargetLetters = selectedTerm['term'].length;
+console.log('number of letters in target: ', numberTargetLetters);
+
+var target = []; // declare target array for display of solution
+for (i=0; i<numberTargetLetters; i++) {
+    target.push(' ');  
+    }
+console.log('initial target has: ' + target.length + ' blank characters');
+
+var termLetters = selectedTerm['term'].split("");
+console.log('termLetters: ', termLetters); 
+
+var termLetterSet = new Set(termLetters);
+console.log('termLetterSet:', termLetterSet);
+
+var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G',
+    'H', 'I', 'J', 'K', 'L', 'M', 'N',
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+
+var alphabetSet = new Set(alphabet);
+console.log('alphabetSet: ', alphabetSet);
+
+var availableLetterSet = new Set(alphabet);
+console.log('availableLetterSet: ', availableLetterSet);
+
+var usedLetterSet = new Set();  // begins as empty set
+console.log('initial usedLetterSet: ', usedLetterSet);
+
+var numberGuessesAllowed = 3;  // hangman rules
+var numberBadGuesses = 0;  // initialize count prior to for-loop
+var continueGame = true;  // Boolean switch use in while loop
+var outcomeMessage = '';  // initialize message showing game outcome
+while ((numberBadGuesses < numberGuessesAllowed) && continueGame) {
+	var promptGuessLetter = prompt('enter a letter from the alphabet');
+    console.log('promptGuessLetter: ', promptGuessLetter);
+	// this must be a single letter... no other characters permitted
+	// we convert the typed letter to uppercase
+    // with each guess we add to the usedLetterSet
+    // and delete from the availableLetterSet 
+    var thisGuessLetter = promptGuessLetter.toUpperCase();
+    console.log('thisGuessLetter: ', thisGuessLetter);
+
+    usedLetterSet.add(thisGuessLetter);
+    console.log('current usedLetterSet: ', usedLetterSet);
+
+    availableLetterSet.delete(thisGuessLetter);
+    console.log('current availableLetterSet:', availableLetterSet);
+    
+    // if the guessLetter is in the termLetterSet then
+    //   we take appropriate action to display updated target
+
+    // if the guessLetter is not in the termLetterSet then
+    //   we increment the numberBadGuesses 
+
+    if (termLetterSet.has(thisGuessLetter)) {
+    	// update the target display by adding correct guess letter
+        for (i=0; i<numberTargetLetters; i++) {
+        	if (thisGuessLetter === termLetters[i]) {
+        		target[i] = thisGuessLetter
+        	}
+        }
+
+    	console.log('current target display:', target);
+
+        // if there are no longer any blank characters in the target
+        // then the game has been won and we pop out of the while-loop
+        var targetSet = new Set(target);
+        if (!targetSet.has(' ')) {
+        	outcomeMessage = 'You Win';
+        	continueGame = false;
+        }
+    }
+    else {
+        numberBadGuesses++;
+        console.log('numberBadGuesses: ', numberBadGuesses)
+    }
+
+} // end of major while-loop
+
+// if finish your alloted numberGuessesAllowed without
+// guessing all the letters... then you lose the game
+// ... but the correct answer is shown with definition
+
+if (numberBadGuesses === numberGuessesAllowed)
+	outcomeMessage = 'Better Luck Next Time';
+
+console.log('----- outcomeMessage:', outcomeMessage)
